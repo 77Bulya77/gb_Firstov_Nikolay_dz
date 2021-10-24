1f3be885c6f24a75e2d8f8bf2a05bad86dff327f
77Bulya77 <92250434+77Bulya77@users.noreply.github.com>
2021-10-24 17:15:31 +0500

    Create naim.py

---
+++ naim.py
@@ -0,0 +1,19 @@
+# def num_translate():
+#   if i in range[english_words()]
+#       return
+
+
+english_words = ['один', 'два', 'три', 'четыре', 'пять', 'шесть', 'семь', 'восемь', 'девять', ]
+russian_dict = {
+    'one': 'один',
+    'two': 'два',
+    'three': 'три',
+    'four': 'четыре',
+    'five': 'пять',
+    'six': 'шесть',
+    'seven': 'семь',
+    'eight': 'восемь',
+    'nine': 'девять',
+    'ten': 'десять'
+}
+print(russian_dict('десять'))
